39eb9da79838b452518057aa51668c0a57f4070c
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2023-11-16 14:00:00 -0500

    [MS-20] Added word crud

--- app.py
+++ app.py
@@ -10,5 +10,21 @@ db = (app)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/word', methods=['POST'])
+def create_word():
+    return 'creating word'
+
+@app.route('/word', methods=['GET'])
+def create_word():
+    return 'reading word'
+
+@app.route('/word', methods=['PUT'])
+def create_word():
+    return 'updating word'
+
+@app.route('/word', methods=['DELETE'])
+def create_word():
+    return 'deleting word'
+
 if __name__ == '__main__':
     app.run(debug=True)
